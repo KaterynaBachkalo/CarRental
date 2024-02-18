7acae9305b8397cbe673ce49145c0559974af350
KaterynaBachkalo <kozlovets88@gmail.com>
2024-02-18 17:01:35 +0200

    edit loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { ThreeDots } from "react-loader-spinner";
+import css from "../Modal/Modal.module.css";
 
 const Loader = () => {
   return (
-    <ThreeDots
-      visible={true}
-      height="80"
-      width="80"
-      color="rgb(52, 112, 255)"
-      radius="9"
-      ariaLabel="three-dots-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-    />
+    <div className={css.spinnerWrapper}>
+      <ThreeDots
+        visible={true}
+        height="80"
+        width="80"
+        color="rgb(52, 112, 255)"
+        radius="12"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{ left: "50%", top: "50%", position: "absolute" }}
+        wrapperClass="modal-wrapper"
+      />
+    </div>
   );
 };
 
