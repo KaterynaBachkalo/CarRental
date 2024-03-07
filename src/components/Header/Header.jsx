b033d3071a2b6c64c6d887190035b13346c78209
KaterynaBachkalo <kozlovets88@gmail.com>
2024-03-07 03:04:24 +0200

    start mobile version for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Iconlogo from "../../img/logo.png";
 import css from "./Header.module.css";
 import { Link, useLocation } from "react-router-dom";
+import { FiAlignRight } from "react-icons/fi";
 
 const Header = () => {
   const location = useLocation();
+  const [isBurgerActive, setBurgerActive] = useState(false);
+
+  const handleBurgerClick = () => {
+    setBurgerActive(!isBurgerActive);
+  };
+
   return (
     <header className={css.header}>
       <div className={css.container}>
@@ -12,41 +19,53 @@ const Header = () => {
           <Link to="/">
             <img className={css.logo} src={Iconlogo} alt="Logo" />
           </Link>
-          <ul className={css.list}>
-            <li>
-              <Link
-                to="/"
-                className={`${css.mainLink} ${
-                  location.pathname === "/" ? css.active : ""
-                }`}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/catalog"
-                className={`${css.catalogLink} ${
-                  location.pathname === "/catalog" ? css.active : ""
-                }`}
-              >
-                Catalog
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/favorites"
-                className={`${css.favoritesLink} ${
-                  location.pathname === "/favorites" ? css.active : ""
-                }`}
-              >
-                Favorites
-              </Link>
-            </li>
-          </ul>
-          <a className={css.tel} href="tel:+380730000000">
-            Call: +380730000000
-          </a>
+
+          <div className={css.wrapperBurger}>
+            <FiAlignRight className={css.burger} onClick={handleBurgerClick} />
+          </div>
+          <div className={css.wrapper}>
+            <div
+              className={`${css.navigationSection} ${
+                isBurgerActive ? css.active : ""
+              }`}
+            >
+              <ul className={css.list}>
+                <li>
+                  <Link
+                    to="/"
+                    className={`${css.mainLink} ${
+                      location.pathname === "/" ? css.active : ""
+                    }`}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/catalog"
+                    className={`${css.catalogLink} ${
+                      location.pathname === "/catalog" ? css.active : ""
+                    }`}
+                  >
+                    Catalog
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/favorites"
+                    className={`${css.favoritesLink} ${
+                      location.pathname === "/favorites" ? css.active : ""
+                    }`}
+                  >
+                    Favorites
+                  </Link>
+                </li>
+              </ul>
+              <a className={css.tel} href="tel:+380730000000">
+                Call: +380730000000
+              </a>
+            </div>
+          </div>
         </nav>
       </div>
     </header>
